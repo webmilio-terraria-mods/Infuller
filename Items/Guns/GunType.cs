b8d95546aa5ef628a19ff17b388da143081ca7ac
webmilio <webmilio@users.noreply.github.com>
2021-11-06 19:25:38 -0400

    Fixed typo in GunType enum.

--- Items/Guns/GunType.cs
+++ Items/Guns/GunType.cs
@@ -3,18 +3,18 @@
 namespace Infuller.Items.Guns;
 
 [Flags]
-public enum GunType : byte
+public enum GunType : ushort
 {
-    Pistol = 0b0000_0001,
-    Shotgun = 0b0000_0010,
-    SMG = 0b0000_0100,
+    Pistol = 0b0001,
+    Shotgun = 0b0010,
+    SMG = 0b0100,
 
+    Cannon = 0b1000,
     Flamethrower = 0b0001_0000,
-    Cannon = 0b0000_1000,
 
     Rifle = 0b0010_0000,
-    SemiAutomaticRifle = Rifle | 0b0001,
-    AutomaticRifle = Rifle | 0b0010,
+    SemiAutomaticRifle = 0b0100_0000 | Rifle,
+    AutomaticRifle = 0b1000_0000 | Rifle,
 
-    Other = 0b1000_0000
+    Other = 0b0001_0000_0000
 }
