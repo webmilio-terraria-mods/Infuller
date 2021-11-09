0056aa7f5919f5e5547a87b8a5017b8d5c4c8d16
webmilio <webmilio@users.noreply.github.com>
2021-11-09 13:32:38 -0500

    Added misc. flails.

--- Items/Melee/Flails.cs
+++ Items/Melee/Flails.cs
@@ -6,10 +6,18 @@ public class Flails : MeleeSystem<IFlail>
 {
     protected override void SetupVanilla()
     {
+        Add(new(WeaponHands.OneHanded),
+            ChainGuillotines,
+            ChainKnife,
+            Flairon);
+
         Add(new(WeaponHands.TwoHanded), 
+            Anchor,
             BallOHurt, BlueMoon,
             DaoofPow, DripplerFlail, 
             FlamingMace, FlowerPow,
+            GolemFist,
+            KOCannon,
             Mace, 
             Sunfury,
             TheMeatball);
